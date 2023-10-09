84afdf6874297be9df9851072f42e7094dfd2324
Nahyun <from.owol@gmail.com>
2023-10-09 16:05:05 +0900

    feat: add registration button ui

--- frontend/src/pages/AddPage/index.jsx
+++ frontend/src/pages/AddPage/index.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import { Button, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const AddPage = () => {
-  return <div>등록 페이지</div>;
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Button
+        variant='outlined'
+        sx={{
+          marginLeft: '100px',
+          color: '#005B41',
+          borderColor: '#005B41',
+          width: '100px',
+          height: '50px',
+          fontSize: '20px',
+          display: ['none', 'none', 'block'],
+        }}
+        onClick={handleBackClick}
+      >
+        이전
+      </Button>
+
+      <Button
+        variant='outlined'
+        sx={{
+          marginRight: '100px',
+          color: '#005B41',
+          borderColor: '#005B41',
+          width: '100px',
+          height: '50px',
+          fontSize: '20px',
+          display: ['none', 'none', 'block'],
+        }}
+      >
+        등록
+      </Button>
+    </Box>
+  );
 };
 
 export default AddPage;
--- frontend/src/pages/MainPage/index.jsx
+++ frontend/src/pages/MainPage/index.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Button, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function MainPage() {
-  return <div>메인 페이지</div>;
+  const naviagte = useNavigate();
+
+  const handleButtonClick = () => {
+    naviagte('/add');
+  };
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'flex-end', // 수평 가운데 정렬
+        alignItems: 'center', // 수직 가운데 정렬
+      }}
+    >
+      <Button
+        variant='outlined'
+        sx={{
+          marginRight: '170px',
+          color: '#005B41',
+          borderColor: '#005B41',
+          width: '100px',
+          height: '50px',
+          fontSize: '20px',
+          display: ['none', 'none', 'block'],
+        }}
+        onClick={handleButtonClick}
+      >
+        등록
+      </Button>
+    </Box>
+  );
 }
 
 export default MainPage;
